feab3bc003a46424b9088ac2b376923fade30f76
CodeingBoy <CodeingBoy@gmail.com>
2017-12-30 03:17:36 +0800

    Modified Users table schema

--- sqls/Table_DDL_Users.sql
+++ sqls/Table_DDL_Users.sql
@@ -1,10 +1,10 @@
 USE OffersWebsite;
 CREATE TABLE Users(
-  ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  Username VARCHAR(20) NOT NULL,
-  HashedPassword TEXT NOT NULL,
-  NickName NVARCHAR(20) NOT NULL,
-  RegisterTime DATETIME NOT NULL DEFAULT NOW(),
-  LastLoginTime DATETIME DEFAULT NOW(),
+  ID                 INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  Username           VARCHAR(20)  NOT NULL,
+  HashedPassword     TEXT         NOT NULL,
+  Nickname           NVARCHAR(20) NOT NULL,
+  RegisterTime       DATETIME     NOT NULL DEFAULT NOW(),
+  LastLoginTime      DATETIME DEFAULT NOW(),
   LastLoginIPAddress VARCHAR(39) -- length of IPv6 address
 );
